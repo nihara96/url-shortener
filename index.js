5d4a22cae615b7e67f0308690e69528f6ddc25b0
nihara <nbmayurawasala@gmail.com>
2023-02-07 18:07:31 +0530

    inital commit

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+require('dotenv').config(); 
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
@@ -18,6 +19,8 @@ app.get('/', function(req, res) {
 app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
+
+app.get(); 
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
